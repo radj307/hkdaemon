d784f4c317909b21e83c2a23d091b817e1be331e
radj307 <radj307@gmail.com>
2022-11-22 13:00:53 -0500

    initial project setup

---
+++ hkdaemon/src/hkdaemon.cpp
@@ -0,0 +1,16 @@
+﻿#include <opt3.hpp>
+
+#include <Windows.h>
+
+int main(const int argc, char** argv)
+{
+	try {
+		opt3::ArgManager args{ argc, argv };
+
+
+
+		return 0;
+	} catch (...) {
+		return 1;
+	}
+}
